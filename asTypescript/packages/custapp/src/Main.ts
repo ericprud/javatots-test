419860879bd21cded77b1e1b36337377fe0692d8
Eric Prud'hommeaux <eric+github@w3.org>
2021-11-20 19:21:34 +0100

    ~ re-order imports

--- asTypescript/packages/custapp/src/Main.ts
+++ asTypescript/packages/custapp/src/Main.ts
@@ -1,20 +1,22 @@
 // Corresponding javatots-test package: org.javatots.example.customerapp
-import { Customer } from '../../../custdb/lib/customerdb/models/Customer';
 import { Cli } from '../../../custdb/lib/customerdb/Cli';
+import { Customer } from '../../../custdb/lib/customerdb/models/Customer';
 import * as Yaml from 'js-yaml';
-import * as Fs from 'fs';
-import { Readable } from 'stream';
 import { Writable } from 'stream';
 
 export class Main {
 
   public static main(args: string[]): void /* throws FileNotFoundException */ {
     const dir: string = "../../../" + "customer-db/src/main/resources/";
-    const c: Customer = new Cli().loadCustomer(dir + "customer.yaml");
+    const c: Customer = loadCustomer(dir + "customer.yaml");
     console.log(c);
     const writer: Writable = new Writable();
     // new Yaml().dump(c, writer);
     // System.out.println(writer);
+  }
+
+  public static loadCustomer(readonly yamlFilePath: string): Customer /* throws FileNotFoundException */ {
+    return new Cli().loadCustomer(yamlFilePath);
   }
 }
 
--- customer-app/src/main/java/org/javatots/example/customerapp/Main.java
+++ customer-app/src/main/java/org/javatots/example/customerapp/Main.java
@@ -1,6 +1,6 @@
 package org.javatots.example.customerapp;
 
-import org.javatots.example.customerdb.*;
+import org.javatots.example.customerdb.Cli;
 import org.javatots.example.customerdb.models.Customer;
 import org.yaml.snakeyaml.Yaml;
 
